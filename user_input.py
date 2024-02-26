28775361a8d38972f6c5571654888ee87ab3377e
kara2024 <mitchellenjerik@gmail.com>
2024-02-26 08:15:28 +0300

    This is a new commit.

---
+++ user_input.py
@@ -0,0 +1,13 @@
+print('What is your name? ')
+name = input()
+
+print('What is your age? ')
+age = input()
+
+print('What is your location? ')
+location = input()
+
+print('Hello', name, 'you are', age, 'years old and live in', location)
+
+
+
